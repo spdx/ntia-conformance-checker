a225ed6e068c9a59347076662ffc69f5046dfb10
John Speed Meyers <jsmeyers@chainguard.dev>
2022-12-30 14:57:05 -0500

    Remove spdx-tools fork from setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,4 @@ setup(
     name="ntia-conformance-checker",
     version="0.0.1",
     packages=["ntia_conformance_checker", "ntia_conformance_checker.cli_tools"],
-    install_requires=[
-        "spdx-tools @ git+https://github.com/linynjosh/tools-python#egg=spdx-tools",
-    ],
 )
