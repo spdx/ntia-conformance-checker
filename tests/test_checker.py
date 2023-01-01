694e9a7b1df2767da04f3583b8a76362f492e8b7
John Speed Meyers <jsmeyers@chainguard.dev>
2023-01-01 12:46:20 -0500

    Refactor test suite
    This commit removes redundant tests and testing patterns better
    suited to the spdx-tools parsing library.
    
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -16,9 +16,7 @@ test_files = [os.path.join(dirname, fn) for fn in os.listdir(dirname)]
 
 @pytest.mark.parametrize("test_file", test_files)
 def test_checker_no_errors(test_file):
-    doc, error = parse_anything.parse_file(test_file)
-    print(str(doc.version) == "SPDX-2.1")
-    assert not error
+    parse_anything.parse_file(test_file)
     assert not check_anything.check_minimum_elements(test_file).messages
 
 
@@ -30,8 +28,7 @@ test_files_missing_author_name = [
 
 @pytest.mark.parametrize("test_file", test_files_missing_author_name)
 def test_checker_missing_author_name(test_file):
-    doc, error = parse_anything.parse_file(test_file)
-    assert not error
+    doc, _ = parse_anything.parse_file(test_file)
     assert check_anything.check_minimum_elements(test_file).messages == [
         str(doc.name) + ": Document has no author."
     ]
@@ -43,81 +40,10 @@ test_files_missing_timestamp = [os.path.join(dirname, fn) for fn in os.listdir(d
 
 @pytest.mark.parametrize("test_file", test_files_missing_timestamp)
 def test_checker_missing_timestamp(test_file):
-    doc, error = parse_anything.parse_file(test_file)
-    assert error
+    doc, _ = parse_anything.parse_file(test_file)
     assert check_anything.check_minimum_elements(test_file).messages == [
         str(doc.name + ": Errors while parsing: True"),
         str(doc.name + ": Document has no timestamp."),
-    ]
-
-
-dirname = os.path.join(os.path.dirname(__file__), "data", "other_tests")
-file = [os.path.join(dirname, "SPDXSBOMExample.spdx.yml")]
-
-
-@pytest.mark.parametrize("test_file", file)
-def test_checker_missing_timestamp_(test_file):
-    _, error = parse_anything.parse_file(test_file)
-    assert error
-    assert check_anything.check_minimum_elements(test_file).messages == [
-        "xyz-0.1.0: Errors while parsing: True",
-        "xyz-0.1.0: Document has no timestamp.",
-        "xyz-0.1.0: xyz has no supplier.",
-        "xyz-0.1.0: curl has no supplier.",
-        "xyz-0.1.0: openssl has no supplier.",
-        "xyz-0.1.0: xyz has no identifier.",
-        "xyz-0.1.0: curl has no identifier.",
-        "xyz-0.1.0: openssl has no identifier.",
-    ]
-
-
-dirname = os.path.join(os.path.dirname(__file__), "data", "missing_supplier_name")
-test_files_missing_supplier_name = [
-    os.path.join(dirname, fn) for fn in os.listdir(dirname)
-]
-
-
-@pytest.mark.parametrize("test_file", test_files_missing_supplier_name)
-def test_checker_missing_author_name_json(test_file):
-    _, error = parse_anything.parse_file(test_file)
-    assert not error
-    assert check_anything.check_minimum_elements(test_file).messages == [
-        "Sample_Document-V2.1: SPDX Translator has no supplier.",
-        "Sample_Document-V2.1: SPDX Translator has no identifier.",
-    ]
-
-
-dirname = os.path.join(os.path.dirname(__file__), "data", "missing_author_name")
-missing_author_name_rdf = [os.path.join(dirname, "SPDXRdfExample.rdf")]
-
-
-@pytest.mark.parametrize("test_file", missing_author_name_rdf)
-def test_checker_missing_author_name_rdf(test_file):
-    _, error = parse_anything.parse_file(test_file)
-    assert not error
-    assert check_anything.check_minimum_elements(test_file).messages == [
-        "Sample_Document-V2.1: Document has no author."
-    ]
-
-
-dirname = os.path.join(os.path.dirname(__file__), "data", "SPDXSBOMExampleTests")
-missing_author_name_yml = [
-    os.path.join(dirname, "SPDXSBOMExampleMissingAuthorName.spdx.yml")
-]
-
-
-@pytest.mark.parametrize("test_file", missing_author_name_yml)
-def test_checker_missing_author_name_yml(test_file):
-    _, error = parse_anything.parse_file(test_file)
-    assert not error
-    assert check_anything.check_minimum_elements(test_file).messages == [
-        "xyz-0.1.0: Document has no author.",
-        "xyz-0.1.0: xyz has no supplier.",
-        "xyz-0.1.0: curl has no supplier.",
-        "xyz-0.1.0: openssl has no supplier.",
-        "xyz-0.1.0: xyz has no identifier.",
-        "xyz-0.1.0: curl has no identifier.",
-        "xyz-0.1.0: openssl has no identifier.",
     ]
 
 
@@ -127,26 +53,9 @@ files = [os.path.join(dirname, fn) for fn in os.listdir(dirname)]
 
 @pytest.mark.parametrize("test_file", files)
 def test_checker_missing_supplier_name(test_file):
-    _, error = parse_anything.parse_file(test_file)
-    assert not error
+    doc, _ = parse_anything.parse_file(test_file)
     assert check_anything.check_minimum_elements(test_file).messages == [
-        "Sample_Document-V2.1: SPDX Translator has no supplier.",
-        "Sample_Document-V2.1: SPDX Translator has no identifier.",
-    ]
-
-
-dirname = os.path.join(os.path.dirname(__file__), "data", "missing_author_name")
-test_files_missing_author_name = [
-    os.path.join(dirname, fn) for fn in os.listdir(dirname)
-]
-
-
-@pytest.mark.parametrize("test_file", test_files_missing_author_name)
-def test_checker_missing_author_name_simple_tag(test_file):
-    _, error = parse_anything.parse_file(test_file)
-    assert not error
-    assert check_anything.check_minimum_elements(test_file).messages == [
-        "Sample_Document-V2.1: Document has no author."
+        f"{doc.name}: {doc.packages[0].name} has no supplier."
     ]
 
 
@@ -157,9 +66,8 @@ test_files_missing_component_version = [
 
 
 @pytest.mark.parametrize("test_file", test_files_missing_component_version)
-def test_checker_missing_version_name_simple_tag(test_file):
-    doc, error = parse_anything.parse_file(test_file)
-    assert not error
+def test_checker_missing_component_version(test_file):
+    doc, _ = parse_anything.parse_file(test_file)
     assert check_anything.check_minimum_elements(test_file).messages == [
         doc.name + ": " + doc.packages[0].name + " has no version."
     ]
@@ -173,9 +81,13 @@ test_files_missing_unique_identifiers = [
 
 @pytest.mark.parametrize("test_file", test_files_missing_unique_identifiers)
 def test_checker_missing_unique_identifiers(test_file):
-    _, _ = parse_anything.parse_file(test_file)
+    # note: it appears to not be possible to check exclusively
+    # for a missing unique identifier. the spdx-tools parsing library
+    # considers missing unique identifiers to be an error
+    doc, _ = parse_anything.parse_file(test_file)
     assert check_anything.check_minimum_elements(test_file).messages == [
-        "Sample_Document-V2.1: Errors while parsing: True"
+        f"{doc.name}: Errors while parsing: True",
+        f"{doc.name}: {doc.packages[0].name} has no identifier.",
     ]
 
 
@@ -189,25 +101,9 @@ test_files_missing_dependency_relationships = [
 
 @pytest.mark.parametrize("test_file", test_files_missing_dependency_relationships)
 def test_checker_missing_dependency_relationships(test_file):
-    doc, error = parse_anything.parse_file(test_file)
-    assert not error
+    doc, _ = parse_anything.parse_file(test_file)
     assert check_anything.check_minimum_elements(test_file).messages == [
         str(doc.name) + ": Document has no dependency relationships."
-    ]
-
-
-dirname = os.path.join(os.path.dirname(__file__), "data", "missing_component_name")
-test_files_missing_component_names = [
-    os.path.join(dirname, fn) for fn in os.listdir(dirname)
-]
-
-
-@pytest.mark.parametrize("test_file", test_files_missing_component_names)
-def test_fossology(test_file):
-    doc, error = parse_anything.parse_file(test_file)
-    assert error
-    assert check_anything.check_minimum_elements(test_file).messages == [
-        str(doc.name + ": Errors while parsing: True")
     ]
 
 
