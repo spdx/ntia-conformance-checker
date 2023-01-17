1b23e9777849c75d09e6081ffadeee800c22d7e1
John Speed Meyers <jsmeyers@chainguard.dev>
2023-01-16 20:25:19 -0500

    Replace setup.py with pyproject.toml
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""Package setup."""
-
-from setuptools import setup
-
-setup(
-    name="ntia-conformance-checker",
-    version="0.0.1",
-    packages=["ntia_conformance_checker", "ntia_conformance_checker.cli_tools"],
-)
