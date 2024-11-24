185fe609dbc1c8beb0e698d28e2ca2089bca7d31
Arthit Suriyawongkul <arthit@gmail.com>
2024-11-24 10:01:04 +0000

    Move NTIA check to ntia_checker.py
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- ntia_conformance_checker/__init__.py
+++ ntia_conformance_checker/__init__.py
@@ -1,3 +1,6 @@
 """Export functions for usage as library."""
 
+__all__ = ["NTIAChecker", "SbomChecker"]
+
+from ntia_conformance_checker.ntia_checker import NTIAChecker
 from ntia_conformance_checker.sbom_checker import SbomChecker
