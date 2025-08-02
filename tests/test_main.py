410eda3b3470e3230bcdd734b0c0a5338f3729e2
Arthit Suriyawongkul <arthit@gmail.com>
2025-08-02 19:24:57 +0100

    Add detect_spdx_Version tests
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

---
+++ tests/test_main.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2025 SPDX contributors
+# SPDX-FileType: SOURCE
+# SPDX-License-Identifier: Apache-2.0
+
+"""Tests functions in main"""
+
+# pylint: disable=missing-function-docstring
+
+from pathlib import Path
+from typing import List, Tuple
+
+from ntia_conformance_checker.main import detect_spdx_version
+
+spdx2_2_dir = Path(__file__).parent / "data" / "missing_component_name"
+spdx2_3_dir = Path(__file__).parent / "data" / "missing_timestamp"
+spdx3_0_dir = Path(__file__).parent / "data" / "spdx3"
+
+
+detect_version_test: List[Tuple[Path, str]] = [
+    (spdx2_2_dir / "SPDXJsonExample.json", "2.2"),
+    (spdx2_2_dir / "SPDXXmlExample.xml", "2.2"),
+    (spdx2_2_dir / "SPDXYamlExample.yaml", "2.2"),
+    (spdx2_3_dir / "SPDXJSONExample-v2.3.spdx.json", "2.3"),
+    (spdx2_3_dir / "SPDXRdfExample-v2.3.spdx.rdf.xml", "2.3"),
+    (spdx2_3_dir / "SPDXTagExample-v2.3.spdx", "2.3"),
+    (spdx2_3_dir / "SPDXXMLExample-v2.3.spdx.xml", "2.3"),
+    (spdx2_3_dir / "SPDXYAMLExample-v2.3.spdx.yaml", "2.3"),
+    (spdx3_0_dir / "dataset-example01.json", "3.0.1"),
+]
+
+
+def test_detect_spdx_version():
+    for file_path, expected_version in detect_version_test:
+        version = detect_spdx_version(str(file_path))
+        assert (
+            version == expected_version
+        ), f"Expected {expected_version}, got {version} for {file_path}"
