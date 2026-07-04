67914ed8711eb99490b466ed515ff7f6eaa17bad
Arthit Suriyawongkul <arthit@gmail.com>
2026-07-04 11:25:47 +0100

    no_element_missing test should be complaint
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -366,8 +366,8 @@ def test_sbomchecker_spdx3_no_elements_missing() -> None:
     assert sbom.doc_author
     assert sbom.doc_timestamp
     assert sbom.sbom_gen_context
-    assert not sbom.dependency_relationships
-    assert not sbom.compliant
+    assert sbom.dependency_relationships
+    assert sbom.compliant
 
 
 def test_sbomchecker_fsct3_spdx3_no_elements_missing() -> None:
@@ -377,8 +377,8 @@ def test_sbomchecker_fsct3_spdx3_no_elements_missing() -> None:
     )
     assert sbom.doc is not None
     assert isinstance(sbom.doc, spdx3.SHACLObjectSet)
-    assert not sbom.dependency_relationships
-    assert not sbom.compliant
+    assert sbom.dependency_relationships
+    assert sbom.compliant
     assert len(sbom.validation_messages) == 0
 
 
